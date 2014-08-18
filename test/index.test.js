351284f51be1002b841d80d71fcd3824e6e8eb25
Jack Boberg <jack.boberg@gmail.com>
2014-08-17 22:01:44 -0400

    Implement example tests

--- test/index.test.js
+++ test/index.test.js
@@ -2,7 +2,7 @@ var expect = require('chai').expect;
 
 var subject = require('../');
 
-var validString = '';
+var validString = '1,3-5,7-9';
 var validRadix  = 10;
 
 describe('index', function() {
@@ -25,6 +25,20 @@ describe('index', function() {
   it('resolves to an array', function() {
     return subject(validString, validRadix).then(function (result) {
       expect(result).to.be.an('array');
+    });
+  });
+
+  describe('examples', function() {
+    it('defaults to radix=10', function() {
+      return subject(validString).then(function (result) {
+        expect(result).to.include.members([1,3,4,5,7,8,9]);
+      });
+    });
+
+    it('supports setting a radix', function() {
+      return subject('10,100-110', 2).then(function (result) {
+        expect(result).to.include.members([2,4,5,6]);
+      });
     });
   });
 });
