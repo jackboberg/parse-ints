4dbc8b1e5b9f9d61cfa11c6bc8e7de4caa0260d5
Jack Boberg <jack.boberg@gmail.com>
2014-12-10 13:56:15 -0500

    Improve/reword basic test
    rename output variable

--- test/usage.test.js
+++ test/usage.test.js
@@ -1,34 +1,41 @@
 var test = require('tape');
 var parseInts = require('../');
 
-test('converts simple string to integer', function (t) {
-  t.plan(1);
-  var ints = parseInts('1');
-  t.equal(ints, 1);
+test('requires a string as the 1st parameter', function (t) {
+  t.plan(2);
+  var output;
+
+  output = parseInts('1');
+  t.equal(output, 1);
+
+  output = parseInts();
+  t.ok(isNaN(output));
 });
 
-test('allows passing a radix', function (t) {
+test('allows passing a radix as 2nd parameter', function (t) {
   t.plan(5);
-  var ints;
+  var output;
 
-  ints = parseInts('1111', 2);
-  t.equal(ints, 15);
+  output = parseInts('1111', 2);
+  t.equal(output, 15);
 
-  ints = parseInts('17', 8);
-  t.equal(ints, 15);
+  output = parseInts('17', 8);
+  t.equal(output, 15);
 
-  ints = parseInts('015', 10);
-  t.equal(ints, 15);
+  output = parseInts('015', 10);
+  t.equal(output, 15);
 
-  ints = parseInts('12', 13);
-  t.equal(ints, 15);
+  output = parseInts('12', 13);
+  t.equal(output, 15);
 
-  ints = parseInts('0xF', 16);
-  t.equal(ints, 15);
+  output = parseInts('0xF', 16);
+  t.equal(output, 15);
 });
 
 test('radix defaults to 10', function (t) {
   t.plan(1);
-  var ints = parseInts('101');
-  t.equal(ints, 101);
+  var output = parseInts('101');
+  t.equal(output, 101);
 });
+
+
