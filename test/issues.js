eac0e9264d89f8393133be477b5c2e35f4b80f37
Jack Boberg <jack.boberg@gmail.com>
2014-12-18 12:08:49 -0500

    Add issues information to README
    clean up test require statements
    use `nonInts.length` to set test plan

--- test/issues.js
+++ test/issues.js
@@ -1,9 +1,9 @@
 var test      = require('tape');
-var parseInts = require('parse-ints');
+var parseInts = require('../');
 
 test.skip('#1 - require radix is an integer or undefined', function (t) {
   var nonInts = ['1', [1], {}];
-  t.plan(nonInts);
+  t.plan(nonInts.length);
   nonInts.forEach(function (radix) {
     t.throws(function () {
       parseInts('1', radix);
